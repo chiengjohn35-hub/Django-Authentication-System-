7b37ff0575a1588565837c22e380abb662419553
Chieng_john <chiengbulgai35@gmail.com>
2025-10-06 08:40:01 -0400

    Delete PythonDjangoProject2 directory

--- PythonDjangoProject2/DjangoApp/project/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Student(models.Model):
-    username =models.CharField(max_length=20)
-    email = models.EmailField(null=False)
-    password = models.IntegerField(null=False)
-
-
